72b1d58433904e6513cc5dcbafbe186ac5bd0964
Shyam Gaurav Venkatesh <shyamgav@usc.edu>
2018-07-23 19:51:29 -0700

    Add Constants

---
+++ Constants.py
@@ -0,0 +1,13 @@
+# Constants without private static final look...wrong.
+
+CASCADE_DIR = 'cascade_files'
+DATA_DIR = './data/'
+
+CASCADE_FILE = 'haarcascade_frontalface_default.xml'
+DATA_IMAGE_FILE = 'converted_images.npy'
+DATA_LABEL_FILE = 'converted_labels.npy'
+DATASET_CSV_FILENAME = 'fer2013.csv'
+
+FACE_SIZE = 48
+EMOTIONS = ['angry', 'disgusted', 'fearful',
+'happy', 'sad', 'surprised', 'neutral']
